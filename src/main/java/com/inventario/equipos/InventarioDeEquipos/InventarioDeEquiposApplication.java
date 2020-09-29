bedba1da6f6147b3acfafa480a9f349d9b805f5b
Jorge Vargas <jorgevargasmateo@hotmail.com>
2020-09-29 09:49:44 -0400

    Project creation

---
+++ src/main/java/com/inventario/equipos/InventarioDeEquipos/InventarioDeEquiposApplication.java
@@ -0,0 +1,13 @@
+package com.inventario.equipos.InventarioDeEquipos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventarioDeEquiposApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventarioDeEquiposApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inventario/equipos/InventarioDeEquipos/InventarioDeEquiposApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inventario.equipos.InventarioDeEquipos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventarioDeEquiposApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
